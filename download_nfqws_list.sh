e673521c40e3f0440fd6560d9197ed3d001cf01e
Nikolay <bespalov.nikolay@yandex.ru>
2025-04-28 10:46:06 +0500

    Update download_nfqws_list.sh

--- download_nfqws_list.sh
+++ download_nfqws_list.sh
@@ -5,9 +5,9 @@ if [ ! -f /tmp/.is_online ]; then
 fi
 
 if [ ! -f /opt/etc/nfqws/domains.list ]; then
-    curl -f -o /opt/etc/nfqws/domains.list \
+    curl -f -L -o /opt/etc/nfqws/domains.list \
         $(curl -s https://api.github.com/repos/1andrevich/Re-filter-lists/releases/latest | grep -o '"browser_download_url": "[^"]*' | grep -o 'https://[^"]*domains_all.lst') > /dev/null 2>&1
 else
-    curl -f -z /opt/etc/nfqws/domains.list -o /opt/etc/nfqws/domains.list \
+    curl -f -L -z /opt/etc/nfqws/domains.list -o /opt/etc/nfqws/domains.list \
         $(curl -s https://api.github.com/repos/1andrevich/Re-filter-lists/releases/latest | grep -o '"browser_download_url": "[^"]*' | grep -o 'https://[^"]*domains_all.lst') > /dev/null 2>&1
 fi
