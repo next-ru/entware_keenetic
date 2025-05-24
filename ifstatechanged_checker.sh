2dd99ef61e677c61f49b1b14f257e63e5c19a762
Nikolay <bespalov.nikolay@yandex.ru>
2025-05-24 09:06:28 +0500

    Update ifstatechanged_checker.sh

--- ifstatechanged_checker.sh
+++ ifstatechanged_checker.sh
@@ -8,22 +8,16 @@ log() {
 
 if [ ! -f "/tmp/.last_boot" ]; then
     touch "/tmp/.last_boot"
-    log "system ready, uptime $(awk '{print int($1)}' /proc/uptime) seconds"
+    log "system boot, uptime $(awk '{print int($1)}' /proc/uptime) seconds"
 fi
 
-if [ "$system_name" = "eth2.2" ]; then
-    if [ "$link" = "up" ]; then
-        if [ ! -f "/tmp/.eth2.2_up" ]; then
-            log "eth2.2 interface is up"
-            touch "/tmp/.eth2.2_up"
-        fi
-    else        
-        if [ -f "/tmp/.eth2.2_up" ]; then
-            log "eth2.2 interface is down"
-            rm -f "/tmp/.eth2.2_up"
-        fi
-    fi
+if [ "$system_name" = "eth2.2" ] && [ "$change" = "link" ]; then
+    log "eth2.2 interface is $link"
 fi
+
+# if [ "$system_name" = "ppp0" ] && [ "$change" = "link" ]; then
+    # log "ppp0 interface is $link"
+# fi
 
 if [ "$system_name" = "ppp0" ]; then
     if [ "$connected" = "yes" ]; then
